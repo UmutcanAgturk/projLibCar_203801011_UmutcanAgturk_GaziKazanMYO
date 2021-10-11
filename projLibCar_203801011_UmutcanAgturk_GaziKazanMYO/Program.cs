d52fceda4bd2210a6232c22151dafb4d5bd1aeed
UMUTCANAGTURK <UMUTCANAGTURK@DESKTOP-ATHICDF>
2021-10-11 17:50:43 +0300

    Add project files.

---
+++ projLibCar_203801011_UmutcanAgturk_GaziKazanMYO/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projLibCar_203801011_UmutcanAgturk_GaziKazanMYO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
