cd3ba8099d730996ddde8bd0ddc09a7441c1052c
Umutcan AĞTÜRK <umutcan20012009@hotmail.com>
2021-10-12 19:45:15 +0300

    1.1.1

--- vehicle/car.cs
+++ vehicle/car.cs
@@ -6,6 +6,21 @@ namespace Vehicle
 {
     class car : vehicle
     {
+        private string Bluetooth;
+        public string bluethooth { get => Bluetooth; set => Bluetooth = value; }
+
+        private string AirConditioning;
+        public string airConditioning { get => AirConditioning; set => AirConditioning = value; }
+
+        private string BodyType;
+        public string bodyType { get => BodyType; set => BodyType = value; }
+
+        private string Variant;
+        public string variant { get => Variant; set => Variant = value; }
+
+        private string Year;
+        public string year { get => Year; set => Year = value; }
 
     }
+    
 }
--- vehicle/vehicle.cs
+++ vehicle/vehicle.cs
@@ -5,6 +5,40 @@ namespace Vehicle
     public class vehicle
     {
 
+        private double FuelCapacity;
+        public double fuelCapacity { get => FuelCapacity; set => FuelCapacity = value; }
+
+        private double FuelConsumption;
+        public double fuelConsumption { get => FuelConsumption; set => FuelConsumption = value; }
+
+        private string FuelTypee;
+        public string fuelTypee { get => FuelTypee; set => FuelTypee = value; }
+
+        private int EngineCapaticy;
+        public int engineCapatcicy { get => EngineCapaticy; set => EngineCapaticy = value; }
+
+        private int Speed;
+        public int speed { get => Speed; set => Speed = value; }
+
+        private byte Door;
+        public double door { get => Door; set => Door = (byte)value; }
+
+        private int SeatingCapaity;
+        public int seatingCapaity { get => SeatingCapaity; set => SeatingCapaity = value; }
+
+        private string Model;
+        public string  model { get => Model; set => Model = value; }
+
+        private string Brand;
+        public string brand { get => Brand; set => Brand = value; }
+
+        private string LuggageCapacity;
+        public string luggageCapaity { get => LuggageCapacity; set => LuggageCapacity = value; }
+
+        private int NumberofWheels;
+        public int numberOfWheels { get => NumberofWheels; set => NumberofWheels = value; }
+
+
 
     }
 }
